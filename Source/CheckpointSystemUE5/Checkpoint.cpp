ebebf1a36e06640f11d425fa8aeb88aed2347685
Pablo Cano San Román <70590054+pablocanosanroman@users.noreply.github.com>
2022-11-01 15:01:27 +0000

    Project Started

---
+++ Source/CheckpointSystemUE5/Checkpoint.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Checkpoint.h"
+
+#include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Particles/ParticleSystemComponent.h"
+
+// Sets default values
+ACheckpoint::ACheckpoint()
+{
+ 	m_Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = m_Root;
+
+	m_CheckpointMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	m_CheckpointMesh->SetupAttachment(m_Root);
+
+	m_CheckpointActivationCheck = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("CheckpointActivationParticle"));
+	m_CheckpointActivationCheck->SetupAttachment(m_Root);
+
+	m_CheckpointCollider = CreateDefaultSubobject<USphereComponent>(TEXT("CheckpointCollider"));
+	m_CheckpointCollider->SetupAttachment(m_Root);
+	
+
+}
+
+void ACheckpoint::BeginPlay()
+{
+	m_CheckpointCollider->OnComponentBeginOverlap.AddDynamic(this, &ACheckpoint::OnOverlapBegin);
+	Super::BeginPlay();
+}
+
+void ACheckpoint::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+                                 UPrimitiveComponent* OtherComp, int32 OtherBodyindex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if(OtherActor->IsA(APawn::StaticClass()))
+	{
+		onCheckpointActivated.Broadcast(OtherActor);
+	}
+}
+
+
+
+
---
+++ Source/CheckpointSystemUE5/Checkpoint.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Checkpoint.generated.h"
+
+class USceneComponent;
+class UStaticMeshComponent;
+class UParticleSystemComponent;
+class USphereComponent;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnCheckpointActivated, AActor*, pawnInstigator);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnCheckpointDeactivated);
+
+UCLASS()
+class CHECKPOINTSYSTEMUE5_API ACheckpoint : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACheckpoint();
+
+	UPROPERTY(BlueprintAssignable, Category = "CheckpointEvents")
+	FOnCheckpointActivated onCheckpointActivated;
+
+	UPROPERTY(BlueprintAssignable, Category= "CheckpoinEvents")
+	FOnCheckpointDeactivated OnCheckpointDeactivated;
+
+protected:
+
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	USceneComponent* m_Root;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UStaticMeshComponent* m_CheckpointMesh;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UParticleSystemComponent* m_CheckpointActivationCheck;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	USphereComponent* m_CheckpointCollider;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyindex, bool bFromSweep, const FHitResult& SweepResult);
+
+};
